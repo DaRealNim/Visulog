c48b1722e07c055cf7bc2fdabd16217e92ac2550
krkn <krkn@pop-os.localdomain>
2020-10-14 10:12:55 +0200

    saveConfigFile working

--- cli/src/main/java/up/visulog/cli/CLILauncher.java
+++ cli/src/main/java/up/visulog/cli/CLILauncher.java
@@ -10,65 +10,87 @@ import java.util.Optional;
 
 import java.util.Scanner;
 import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class CLILauncher {
-    public static void main(String[] args) {
-        var config = makeConfigFromCommandLineArgs(args);
-        if (config.isPresent()) {
-            var analyzer = new Analyzer(config.get());
-            var results = analyzer.computeResults();
-            System.out.println(results.toHTML());
-        } else displayHelpAndExit();
-    }
+	public static void main(String[] args) {
+		var config = makeConfigFromCommandLineArgs(args);
+		if (config.isPresent()) {
+			var analyzer = new Analyzer(config.get());
+			var results = analyzer.computeResults();
+			System.out.println(results.toHTML());
+		} else displayHelpAndExit();
+	}
 
-    static Optional<Configuration> makeConfigFromCommandLineArgs(String[] args) {
-        var gitPath = FileSystems.getDefault().getPath(".");
-        var plugins = new HashMap<String, PluginConfig>();
-        for (var arg : args) {
-            if (arg.startsWith("--")) {
-                String[] parts = arg.split("=");
-                if (parts.length != 2) 
-									return Optional.empty();
-                else {
-                    String pName = parts[0];
-                    String pValue = parts[1];
-                    switch (pName) {
-                        case "--addPlugin":
-														plugins.put(pValue, new PluginConfig(){}); //Adds plugin passed.
-                            break;
-                        case "--loadConfigFile":
-                            // TODO (load options from a file)
-                            break;
-                        case "--justSaveConfigFile":
-													try //necessary as BufferedWriter throws IOException
-													{	
-														BufferedWriter writer = new BufferedWriter(new FileWriter(pValue));
-														for (var cfg : args) //Iterates through options
-															if (!(cfg.equals("--justSaveConfigFile=" + pValue))) //Does not write save option to file.
-																writer.write(cfg + " ");
-														writer.close();
-													}
-													catch (IOException ex)
-													{
-														System.out.println("Something went wrong.");
-													}
-                          break;
-                        default:
-                            return Optional.empty();
-                    }
-                }
-            } else {
-                gitPath = FileSystems.getDefault().getPath(arg);
-            }
-        }
-        return Optional.of(new Configuration(gitPath, plugins));
-    }
+	static Optional<Configuration> makeConfigFromCommandLineArgs(String[] args) {
+		var gitPath = FileSystems.getDefault().getPath(".");
+		var plugins = new HashMap<String, PluginConfig>();
+		for (var arg : args) {
+			if (arg.startsWith("--")) {
+				String[] parts = arg.split("=");
+				if (parts.length != 2) 
+					return Optional.empty();
+				else {
+					String pName = parts[0];
+					String pValue = parts[1];
+					switch (pName) {
+						case "--addPlugin":
+							plugins.put(pValue, new PluginConfig(){}); //Adds plugin passed.
+							break;
+						case "--loadConfigFile":
+							try {
+								BufferedReader reader = new BufferedReader(new FileReader(pValue));
+								String fileConfig = ""; 
+								boolean fileNotRead = true;
 
-    private static void displayHelpAndExit() {
-        System.out.println("Wrong command...");
-        //TODO: print the list of options and their syntax
-        System.exit(0);
-    }
+								while (fileNotRead) { //Reads each argument and adds to config table
+									String iterationArgument = reader.readLine();
+									String iterationArgumentParts[] = iterationArgument.split("=");
+
+									if (!(iterationArgumentParts[0] == "--loadConfigFile")) //Ignores load argument
+										fileConfig += iterationArgument + " ";
+
+									if (iterationArgument == null)
+										fileNotRead = !fileNotRead;
+								}
+
+								String[] fileConfigTable = fileConfig.split(" ");
+
+								makeConfigFromCommandLineArgs(fileConfigTable);
+							}
+							catch (IOException e) {
+								System.out.println("Something went wrong.");
+							}
+							break;
+						case "--justSaveConfigFile":
+							try {	
+								BufferedWriter writer = new BufferedWriter(new FileWriter(pValue));
+								for (var cfg : args) //Iterates through options
+									if (!(cfg.equals("--justSaveConfigFile=" + pValue))) //Does not write save option to file.
+										writer.write(cfg + "\n");
+								writer.close();
+							}
+							catch (IOException ex) {
+								System.out.println("Something went wrong.");
+							}
+							break;
+						default:
+							return Optional.empty();
+					}
+				}
+			} else {
+				gitPath = FileSystems.getDefault().getPath(arg);
+			}
+		}
+		return Optional.of(new Configuration(gitPath, plugins));
+	}
+
+	private static void displayHelpAndExit() {
+		System.out.println("Wrong command...");
+		//TODO: print the list of options and their syntax
+		System.exit(0);
+	}
 }
