f9b06c66e151385c77e659abf472880deacfcc0d
Nouredine <in_lamamra@esi.dz>
2020-10-19 23:47:47 +0100

    PluginConfig now extend Map

--- config/src/main/java/up/visulog/config/PluginConfig.java
+++ config/src/main/java/up/visulog/config/PluginConfig.java
@@ -1,5 +1,8 @@
 package up.visulog.config;
 
-// TODO: define what this type should be (probably a Map: settingKey -> settingValue)
-public interface PluginConfig {
+import java.util.Map;
+
+// cette interface se comporte comme une Map de couple (clé , valeur).
+public interface PluginConfig extends Map<String, Object> {
+
 }
