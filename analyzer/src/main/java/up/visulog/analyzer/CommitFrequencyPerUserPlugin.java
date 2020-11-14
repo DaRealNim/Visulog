6616792a36e8a1096e943c5cccaae353fd9b7d34
Jacqueline Jin <jacqueline.jjin@gmail.com>
2020-11-14 15:08:29 +0100

    add innerclass Result for CommitFrequencyPerUserPlugin

--- analyzer/src/main/java/up/visulog/analyzer/CommitFrequencyPerUserPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CommitFrequencyPerUserPlugin.java
@@ -17,11 +17,12 @@ public class CommitFrequencyPerUserPlugin extends Plugin {
 	//TODO 1(globale, renvoit le result de la méthode run): récupérer tous les commits dans une List<Commit>,
 	//puis parcourir la List<Commit> pour appeler d'autres méthodes aux(calcul période) pour chaque auteur
 	public Result frequencyPerUser(List<Commit> gitLog) {
-		List<String> authorsDone=new ArrayList<String>();
+		var result = new Result();
+		List<String> authorsDone = new ArrayList<String>(); //list of authors who has commits
 		for(Commit i : gitLog) {
 			if(authorsDone.isEmpty() || !authorsDone.contains(i.author)) {
-				List<Commit> authorCommits=new ArrayList<Commit>();
-				authorCommits=getCommitForThisAuthor(gitLog,i.author);
+				//create List<Commit> for this author
+				List<Commit> authorCommits = getCommitForThisAuthor(gitLog,i.author);
 				authorsDone.add(i.author);
 				timeAverage(tabTimeAverage(authorCommits)); // à stocker dans result;
 			}
@@ -31,7 +32,7 @@ public class CommitFrequencyPerUserPlugin extends Plugin {
 
 	//get in a list all the author's commits
 	public List<Commit> getCommitForThisAuthor(List<Commit> gitLog, String name) {
-		List<Commit> authorCommits=new ArrayList<Commit>();
+		List<Commit> authorCommits = new ArrayList<Commit>();
 		for(Commit i : gitLog) {
 			if(i.author.equals(name)) authorCommits.add(i);
 		}
@@ -52,12 +53,12 @@ public class CommitFrequencyPerUserPlugin extends Plugin {
 		double[] tab;
 		int size=authorCommits.size();
 		if(authorCommits.size()==1) {
-			tab=new double[1];
-			tab[0]=0;
+			tab = new double[1];
+			tab[0] = 0;
 		}
 		tab = new double[authorCommits.size()/2];
-		int index=0;
-		for(int i=0;i<authorCommits.size();i++) {
+		int index = 0;
+		for(int i=0; i<authorCommits.size(); i++) {
 			if(authorCommits.get(i)!=authorCommits.get(authorCommits.size()-1)) {
 			tab[index] = timeBetweenTwoCommits(authorCommits.get(i),authorCommits.get(i+1));
 			index++;
@@ -80,6 +81,22 @@ public class CommitFrequencyPerUserPlugin extends Plugin {
 	@Override
 	public Result getResult() {
 		return null;
+	}
+	
+	static class Result implements AnalyzerPlugin.Result {
+
+		@Override
+		public String getResultAsString() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String getResultAsHtmlDiv() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	
 	}
 
 }
