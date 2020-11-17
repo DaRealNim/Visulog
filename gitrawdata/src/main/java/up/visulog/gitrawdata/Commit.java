36ad872b51aec470a7c399a35085a75fd65c6bb1
Nouredine <in_lamamra@esi.dz>
2020-11-17 22:44:41 +0100

    add some changes to run the code

--- gitrawdata/src/main/java/up/visulog/gitrawdata/Commit.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/Commit.java
@@ -25,10 +25,10 @@ public class Commit {
         this.mergedFrom = mergedFrom;
     }
 
-    // TODO: factor this out (similar code will have to be used for all git commands)
+    // TODO: factor this out (similar code will have to be used for all git
+    // commands)
     public static List<Commit> parseLogFromCommand(Path gitPath) {
-        ProcessBuilder builder =
-                new ProcessBuilder("git", "log").directory(gitPath.toFile());
+        ProcessBuilder builder = new ProcessBuilder("git", "log").directory(gitPath.toFile());
         Process process;
         try {
             process = builder.start();
@@ -51,16 +51,19 @@ public class Commit {
     }
 
     /**
-     * Parses a log item and outputs a commit object. Exceptions will be thrown in case the input does not have the proper format.
-     * Returns an empty optional if there is nothing to parse anymore.
+     * Parses a log item and outputs a commit object. Exceptions will be thrown in
+     * case the input does not have the proper format. Returns an empty optional if
+     * there is nothing to parse anymore.
      */
     public static Optional<Commit> parseCommit(BufferedReader input) {
         try {
 
             var line = input.readLine();
-            if (line == null) return Optional.empty(); // if no line can be read, we are done reading the buffer
+            if (line == null)
+                return Optional.empty(); // if no line can be read, we are done reading the buffer
             var idChunks = line.split(" ");
-            if (!idChunks[0].equals("commit")) parseError();
+            if (!idChunks[0].equals("commit"))
+                parseError();
             var builder = new CommitBuilder(idChunks[1]);
 
             line = input.readLine();
@@ -80,14 +83,18 @@ public class Commit {
                         break;
                     default: // TODO: warn the user that some field was ignored
                 }
-                line = input.readLine(); //prepare next iteration
-                if (line == null) parseError(); // end of stream is not supposed to happen now (commit data incomplete)
+                line = input.readLine(); // prepare next iteration
+                if (line == null)
+                    parseError(); // end of stream is not supposed to happen now (commit data incomplete)
             }
 
             // now read the commit message per se
-            var description = input
-                    .lines() // get a stream of lines to work with
-                    .takeWhile(currentLine -> !currentLine.isEmpty()) // take all lines until the first empty one (commits are separated by empty lines). Remark: commit messages are indented with spaces, so any blank line in the message contains at least a couple of spaces.
+            var description = input.lines() // get a stream of lines to work with
+                    .takeWhile(currentLine -> !currentLine.isEmpty()) // take all lines until the first empty one
+                                                                      // (commits are separated by empty lines). Remark:
+                                                                      // commit messages are indented with spaces, so
+                                                                      // any blank line in the message contains at least
+                                                                      // a couple of spaces.
                     .map(String::trim) // remove indentation
                     .reduce("", (accumulator, currentLine) -> accumulator + currentLine); // concatenate everything
             builder.setDescription(description);
@@ -98,19 +105,24 @@ public class Commit {
         return Optional.empty(); // this is supposed to be unreachable, as parseError should never return
     }
 
-    // Helper function for generating parsing exceptions. This function *always* quits on an exception. It *never* returns.
+    // Helper function for generating parsing exceptions. This function *always*
+    // quits on an exception. It *never* returns.
     private static void parseError() {
         throw new RuntimeException("Wrong commit format.");
     }
 
     @Override
     public String toString() {
-        return "Commit{" +
-                "id='" + id + '\'' +
-                (mergedFrom != null ? ("mergedFrom...='" + mergedFrom + '\'') : "") + //TODO: find out if this is the only optional field
-                ", date='" + date + '\'' +
-                ", author='" + author + '\'' +
-                ", description='" + description + '\'' +
-                '}';
+        return "Commit{" + "id='" + id + '\'' + (mergedFrom != null ? ("mergedFrom...='" + mergedFrom + '\'') : "") + // TODO:
+                                                                                                                      // find
+                                                                                                                      // out
+                                                                                                                      // if
+                                                                                                                      // this
+                                                                                                                      // is
+                                                                                                                      // the
+                                                                                                                      // only
+                                                                                                                      // optional
+                                                                                                                      // field
+                ", date='" + date + '\'' + ", author='" + author + '\'' + ", description='" + description + '\'' + '}';
     }
 }
