c61b6efc846ec44ff2e2b5ca51ae3684f326e7e8
DaRealNim <darealnim@gmail.com>
2020-12-02 00:24:21 +0100

    Prototypes de graphes lignes et donuts

--- webgen/src/main/java/up/visulog/webgen/Webgen.java
+++ webgen/src/main/java/up/visulog/webgen/Webgen.java
@@ -1,10 +1,10 @@
-package up.visulog.webgen;
+// package up.visulog.webgen;
 
 import java.awt.Color;
 
 public class Webgen {
 
-    public static String generateBarGraph(String name, String[] labels, int[] data, Color[] barInsideColors, Color[] barOuterColors) {
+    public static String generateBarGraph(String name, String[] labels, int[] data, Color[] colors) {
         String returnedHTMLCode = "<canvas id='" + name.replace("'", "\\'") + "'></canvas>";
         returnedHTMLCode += "<script>var ctx = document.getElementById('" + name.replace("'", "\\'") + "').getContext('2d');";
         returnedHTMLCode += "var myChart = new Chart(ctx, { type: 'bar', data: { labels: [";
@@ -16,14 +16,53 @@ public class Webgen {
             returnedHTMLCode += String.valueOf(height) + ", ";
         }
         returnedHTMLCode += "], backgroundColor: [";
-        for(Color color : barInsideColors) {
-            returnedHTMLCode += "'rgba(" + String.valueOf(color.getRed()) + ", " + String.valueOf(color.getGreen()) + ", " + String.valueOf(color.getBlue()) + ", " + String.valueOf(color.getAlpha()/255.0) +")', ";
+        for(Color color : colors) {
+            returnedHTMLCode += "'rgba(" + String.valueOf(color.getRed()) + ", " + String.valueOf(color.getGreen()) + ", " + String.valueOf(color.getBlue()) + ", 0.2)', ";
+        }
+        returnedHTMLCode += "], borderColor: [";
+        for(Color color : colors) {
+            returnedHTMLCode += "'rgba(" + String.valueOf(color.getRed()) + ", " + String.valueOf(color.getGreen()) + ", " + String.valueOf(color.getBlue()) + ", 1)', ";
+        }
+        returnedHTMLCode += "], borderWidth : 1 }]}, options: { scales: { yAxes: [{ ticks: { beginAtZero: true }}]}}});</script>\n";
+        return returnedHTMLCode;
+    }
+
+    public static String generateLineGraph(String name, String[] labels, int[] data, Color color) {
+        String returnedHTMLCode = "<canvas id='" + name.replace("'", "\\'") + "'></canvas>";
+        returnedHTMLCode += "<script>var ctx = document.getElementById('" + name.replace("'", "\\'") + "').getContext('2d');";
+        returnedHTMLCode += "var myChart = new Chart(ctx, { type: 'line', data: { labels: [";
+        for(String label : labels) {
+            returnedHTMLCode += "'"+label.replace("'", "\\'")+"', ";
+        }
+        returnedHTMLCode += "], datasets: [{ label: '" + name.replace("'", "\\'") + "', data: [";
+        for(int height : data) {
+            returnedHTMLCode += String.valueOf(height) + ", ";
+        }
+        returnedHTMLCode += "], backgroundColor: 'rgba(" + String.valueOf(color.getRed()) + ", " + String.valueOf(color.getGreen()) + ", " + String.valueOf(color.getBlue()) + ", 0.2)'";
+        returnedHTMLCode += ", borderWidth : 1 }]}, options: { scales: { yAxes: [{ ticks: { beginAtZero: true }}]}}});</script>\n";
+        return returnedHTMLCode;
+    }
+
+    public static String generateDoughnutGraph(String name, String[] labels, int[] data, Color[] colors) {
+        String returnedHTMLCode = "<canvas id='" + name.replace("'", "\\'") + "'></canvas>";
+        returnedHTMLCode += "<script>var ctx = document.getElementById('" + name.replace("'", "\\'") + "').getContext('2d');";
+        returnedHTMLCode += "var myChart = new Chart(ctx, { type: 'doughnut', data: { labels: [";
+        for(String label : labels) {
+            returnedHTMLCode += "'"+label.replace("'", "\\'")+"', ";
+        }
+        returnedHTMLCode += "], datasets: [{ label: '" + name.replace("'", "\\'") + "', data: [";
+        for(int height : data) {
+            returnedHTMLCode += String.valueOf(height) + ", ";
+        }
+        returnedHTMLCode += "], backgroundColor: [";
+        for(Color color : colors) {
+            returnedHTMLCode += "'rgba(" + String.valueOf(color.getRed()) + ", " + String.valueOf(color.getGreen()) + ", " + String.valueOf(color.getBlue()) + ", 0.2)', ";
         }
         returnedHTMLCode += "], borderColor: [";
         for(Color color : barOuterColors) {
-            returnedHTMLCode += "'rgba(" + String.valueOf(color.getRed()) + ", " + String.valueOf(color.getGreen()) + ", " + String.valueOf(color.getBlue()) + ", " + String.valueOf(color.getAlpha()/255.0) +")', ";
+            returnedHTMLCode += "'rgba(" + String.valueOf(color.getRed()) + ", " + String.valueOf(color.getGreen()) + ", " + String.valueOf(color.getBlue()) + ", 1)', ";
         }
-        returnedHTMLCode += "], borderWidth : 1 }]}, options: { scales: { yAxes: [{ ticks: { beginAtZero: true }}]}}});</script>\n";
+        returnedHTMLCode += "], borderWidth : 1 }]}, options: {}});</script>\n";
         return returnedHTMLCode;
     }
 }
