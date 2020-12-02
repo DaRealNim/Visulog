a75cc64986e8c182167a822445c71454208a0224
Nouredine <in_lamamra@esi.dz>
2020-12-02 10:34:47 +0100

    fix merge conflicts after the merge

--- analyzer/src/main/java/up/visulog/analyzer/ActivityPerBranchPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/ActivityPerBranchPlugin.java
@@ -8,52 +8,50 @@ import up.visulog.config.Configuration;
 import up.visulog.gitrawdata.BranchCommits;
 
 public class ActivityPerBranchPlugin extends Plugin {
-	private Result result;
+    private Result result;
 
-	public ActivityPerBranchPlugin(Configuration generalConfiguration) {
-		super(generalConfiguration);
-	}
+    public ActivityPerBranchPlugin(Configuration generalConfiguration) {
+        super(generalConfiguration);
+    }
 
+    @Override
+    public void run() {
+        result = new Result(BranchCommits.countCommitsPerBranch(configuration.getGitPath()));
+    }
 
+    @Override
+    public Result getResult() {
+        if (result == null)
+            run();
+        return result;
+    }
 
-	@Override
-	public void run() {
-		result = new Result(BranchCommits.countCommitsPerBranch(configuration.getGitPath()));
-	}
+    static class Result implements AnalyzerPlugin.Result {
+        private final Map<String, Integer> nbCommitsPerBranch = new HashMap<>();
 
-	@Override
-	public Result getResult() {
-		if (result == null)
-			run();
-		return result;
-	}
+        private Result(List<BranchCommits> branchCommits) {
+            for (BranchCommits branchCommit : branchCommits) {
+                nbCommitsPerBranch.put(branchCommit.getNomDeLaBranche(), branchCommit.getNbCommits());
+            }
+        }
 
-	static class Result implements AnalyzerPlugin.Result {
-		private final Map<String, Integer> nbCommitsPerBranch = new HashMap<>();
+        public Map<String, Integer> getNbCommitsPerBranch() {
+            return nbCommitsPerBranch;
+        }
 
-		private Result(List<BranchCommits> branchCommits) {
-			for (BranchCommits branchCommit : branchCommits) {
-				nbCommitsPerBranch.put(branchCommit.getNomDeLaBranche(), branchCommit.getNbCommits());
-			}
-		}
+        @Override
+        public String getResultAsString() {
+            return nbCommitsPerBranch.toString();
+        }
 
-		public Map<String, Integer> getNbCommitsPerBranch() {
-			return nbCommitsPerBranch;
-		}
-
-		@Override
-		public String getResultAsString() {
-			return nbCommitsPerBranch.toString();
-		}
-
-		@Override
-		public String getResultAsHtmlDiv() {
-			StringBuilder html = new StringBuilder("<div>Number of Commits per branch: <ul>");
-			for (var item : nbCommitsPerBranch.entrySet()) {
-				html.append("<li>").append(item.getKey()).append(": ").append(item.getValue()).append("</li>");
-			}
-			html.append("</ul></div>");
-			return html.toString();
-		}
-	}
+        @Override
+        public String getResultAsHtmlDiv() {
+            StringBuilder html = new StringBuilder("<div>Number of Commits per branch: <ul>");
+            for (var item : nbCommitsPerBranch.entrySet()) {
+                html.append("<li>").append(item.getKey()).append(": ").append(item.getValue()).append("</li>");
+            }
+            html.append("</ul></div>");
+            return html.toString();
+        }
+    }
 }
