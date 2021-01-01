f4a8c674e6e2c9b03abfd19ec72667c3a2af5ad7
DaRealNim <darealnim@gmail.com>
2021-01-01 18:29:07 +0100

    CountCommitsPerAuthor should only return 1 chart

--- analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
@@ -76,10 +76,7 @@ public class CountCommitsPerAuthorPlugin extends Plugin {
             for(int i=0; i<labelsArray.length; i++) labelsArray[i] = labels.get(i);
             for(int i=0; i<dataArray.length; i++) dataArray[i] = data.get(i);
             return new Webgen.Graph[]{
-                new Webgen.BarGraph("<i class=\"fas fa-pen-fancy\"></i>","Commits per author - Bar", labelsArray, new String[]{"Commits per author - Bar"}, new int[][]{dataArray}, new Color[][]{Webgen.generateRandomColorArray(dataArray.length)}),
-                new Webgen.BarGraph("<i class=\"fas fa-pen-fancy\"></i>","Commits per author 1 - Bar", labelsArray, new String[]{"Commits per author - Bar"}, new int[][]{dataArray}, new Color[][]{Webgen.generateRandomColorArray(dataArray.length)}),
-                new Webgen.BarGraph("<i class=\"fas fa-pen-fancy\"></i>","Commits per author 2 - Bar", labelsArray, new String[]{"Commits per author - Bar"}, new int[][]{dataArray}, new Color[][]{Webgen.generateRandomColorArray(dataArray.length)})
-
+                new Webgen.BarGraph("<i class=\"fas fa-pen-fancy\"></i>","Commits per author - Bar", labelsArray, new String[]{"Commits per author - Bar"}, new int[][]{dataArray}, new Color[][]{Webgen.generateRandomColorArray(dataArray.length)})
             };
         }
 
